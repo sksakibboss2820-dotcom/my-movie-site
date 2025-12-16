43a0b2aa2c2951386da83385d1dde231ca8edb5b
sksakibboss2820-dotcom <sksakibboss2820@gmail.com>
2025-12-16 18:50:45 +0600

    Fixed wildcard path error

--- server.js
+++ server.js
@@ -42,9 +42,8 @@ app.get('/add-sample', async (req, res) => {
     }
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
